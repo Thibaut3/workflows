81dac17bcb4e3786d273a2e388e08df3a60af522
Thibaut3 <ThibautJuillard21@gmail.com>
2024-04-16 11:50:43 +0200

    work

---
+++ app.py
@@ -0,0 +1,13 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    # Exemple d'utilisation des fonctions
+    result_addition = addition(5, 3)
+    print("Résultat de l'addition :", result_addition)
+
+    result_subtraction = subtraction(5, 3)
+    print("Résultat de la soustraction :", result_subtraction)
